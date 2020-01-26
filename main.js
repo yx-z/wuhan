28302c6781c65af2ddce11ec490a093be91520a3
yzhng223 <yzhng223@illinois.edu>
2020-01-26 08:21:48 -0500

    Update articles

--- main.js
+++ main.js
@@ -10,48 +10,32 @@ function toDate(timestamp) {
 }
 
 $(document).ready(() => {
-	// $.ajax({
-	// 	type: "GET",
-	// 	url: "http://www.ecdc.europa.eu/en/geographical-distribution-2019-ncov-cases",
-	// 	xhrFields: { withCredentials: false },
-	// 	success: (data) => {
-	// 		console.log(data);
-	// 	},
-	// });
-
-	$.ajax("https://tools.cdc.gov/api/v2/resources/media", {
-		type: "GET",
-		dataType: "jsonp",
-		success: (data) => {
-			let ncovData = data["results"]
-				.filter(obj => obj["description"].toLowerCase().includes("ncov"))
-				.map(obj => {
-					let url = obj["sourceUrl"];
-					let header = obj["description"];
-					let date = Date.parse(obj["dateContentUpdated"]);
-					return [date, `<a href="${url}" target="_blank">${header}</a><br/>`];
-				})
-				.sort((p1, p2) => p2[0] - p1[0])
-				.map(p => `${toDate(p[0])} - ${p[1]}`);
-			$("#content-cdc").html(ncovData.join("<br/>"));
-		}
+	// articles
+	$.when(
+		$.ajax("https://tools.cdc.gov/api/v2/resources/media", {
+			type: "GET", dataType: "jsonp"
+		}),
+		$.ajax("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=ncov&api-key=YYIVck53KehBzDgNXkUSZBxNK8QMpjwu", {
+			type: "GET", dataType: "json"
+		})
+	).done((cdcData, nytData) => {
+		let cdcNcovData = cdcData[0]["results"]
+			.filter(obj => obj["description"].toLowerCase().includes("ncov"))
+			.map(obj => {
+				let url = obj["sourceUrl"];
+				let header = obj["description"];
+				let date = Date.parse(obj["dateContentUpdated"]);
+				return [date, `<a href="${url}" target="_blank">${header}</a><br/>`, "CDC"];
+			});
+		let nytNcovData = nytData[0]["response"]["docs"]
+			.map(obj => {
+				let url = obj["web_url"];
+				let header = obj["abstract"];
+				let date = Date.parse(obj["pub_date"]);
+				return [date, `<a href="${url}" target="_blank">${header}</a><br/>`, "NYTimes"];
+			});
+		let concated = cdcNcovData.concat(nytNcovData).sort((p1, p2) => p2[0] - p1[0])
+			.map(p => `${p[2]}, ${toDate(p[0])} - ${p[1]}`);
+		$("#articles").html(concated);
 	});
-
-	// please don't abuse the api key...
-	$.ajax("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=ncov&api-key=YYIVck53KehBzDgNXkUSZBxNK8QMpjwu", {
-		type: "GET",
-		dataType: "json",
-		success: (data) => {
-			let ncovData = data["response"]["docs"]
-				.map(obj => {
-					let url = obj["web_url"];
-					let header = obj["abstract"];
-					let date = Date.parse(obj["pub_date"]);
-					return [date, `<a href="${url}" target="_blank">${header}</a><br/>`];
-				})
-				.sort((p1, p2) => p2[0] - p1[0])
-				.map(p => `${toDate(p[0])} - ${p[1]}`);
-			$("#content-nyt").html(ncovData.join("<br/>"));
-		}
-	})
 });
