0692a42ccf23c45c9ec62bf89e357f620aa71df0
yzhng223 <yzhng223@illinois.edu>
2020-01-25 21:41:23 -0500

    cdc date

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
+	'Oct', 'Nov', 'Dec'];
+
+function timeConverter(timestamp) {
+	let d = new Date(timestamp);
+	let year = d.getFullYear();
+	let month = months[d.getMonth()];
+	let date = d.getDate();
+	return `${month} ${date}, ${year}`;
+}
+
 $(document).ready(() => {
 	$.ajax("https://tools.cdc.gov/api/v2/resources/media", {
 		type: "GET",
 		dataType: "jsonp",
 		success: (data) => {
-			ncovData = data["results"]
+			console.log(data);
+			let ncovData = data["results"]
 				.filter(obj => obj["description"].toLowerCase().includes("ncov"))
-				.map(obj => `<a href="${obj['sourceUrl']}" target="_blank">${obj["description"]}</a><br/>`);
+				.map(obj => {
+					let url = obj["sourceUrl"];
+					let header = obj["description"];
+					let date = Date.parse(obj["dateContentUpdated"]);
+					return [date, `<a href="${url}" target="_blank">${header}</a><br/>`];
+				})
+				.sort((p1, p2) => p1[0] - p2[0])
+				.map(p => `${timeConverter(p[0])} - ${p[1]}`);
+			console.log(ncovData);
 			$("#content-cdc").html(ncovData.join("<br/>"));
 		}
 	});
@@ -15,7 +35,8 @@ $(document).ready(() => {
 		type: "GET",
 		dataType: "json",
 		success: (data) => {
-			ncovData = data["response"]["docs"].map(obj =>
+			console.log(data);
+			let ncovData = data["response"]["docs"].map(obj =>
 				`<a href="${obj['web_url']}" target="_blank">${obj["abstract"]}</a><br/>`
 			);
 			$("#content-nyt").html(ncovData.join("<br/>"));
