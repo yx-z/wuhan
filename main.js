74c240087155e42678823fb2d4db04d534f69ab4
yzhng223 <yzhng223@illinois.edu>
2020-01-26 11:29:43 -0500

    Update number

--- main.js
+++ main.js
@@ -1,15 +1,52 @@
-const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
-	'Oct', 'Nov', 'Dec'];
+const MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+	"Oct", "Nov", "Dec"];
 
-function toDate(timestamp) {
+const toDate = (timestamp) => {
 	let d = new Date(timestamp);
 	let year = d.getFullYear();
 	let month = MONTHS[d.getMonth()];
 	let date = d.getDate();
 	return `${month} ${date}, ${year}`;
-}
+};
+
+const toLink = (dict, urlKey, headerKey, dateKey, source) => {
+	let url = dict[urlKey];
+	let header = dict[headerKey];
+	let date = Date.parse(dict[dateKey]);
+	return [date, `<a href="${url}" target="_blank">${header}</a><br/>`, source];
+};
+
+
+const corsReq = (target, onSuccess) => {
+	let req = new XMLHttpRequest();
+	req.open(target.method, "https://cors-anywhere.herokuapp.com/" + target.url);
+	req.onload = req.onerror = function () {
+		onSuccess(
+			target.method + ' ' + target.url + '\n' +
+			req.status + ' ' + req.statusText + '\n\n' +
+			(req.responseText || '')
+		);
+	};
+	if (/^POST/i.test(target.method)) {
+		req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	}
+	req.send(target.data);
+};
 
 $(document).ready(() => {
+	// situation
+	corsReq({
+		url: "https://www.ecdc.europa.eu/en/geographical-distribution-2019-ncov-cases",
+		method: "GET"
+	}, (data) => {
+		let html = $.parseHTML(data);
+		let situation = $(html).find(".field .field--item")[0];
+		$(situation).find("h4").remove();
+		$(situation).find("h2").next().remove();
+		$(situation).find("h2").remove();
+		$("#situation").html(situation.outerHTML);
+	});
+
 	// articles
 	$.when(
 		$.ajax("https://tools.cdc.gov/api/v2/resources/media", {
@@ -21,19 +58,9 @@ $(document).ready(() => {
 	).done((cdcData, nytData) => {
 		let cdcNcovData = cdcData[0]["results"]
 			.filter(obj => obj["description"].toLowerCase().includes("ncov"))
-			.map(obj => {
-				let url = obj["sourceUrl"];
-				let header = obj["description"];
-				let date = Date.parse(obj["dateContentUpdated"]);
-				return [date, `<a href="${url}" target="_blank">${header}</a><br/>`, "CDC"];
-			});
+			.map(obj => toLink(obj, "sourceUrl", "description", "dateContentUpdated", "CDC"));
 		let nytNcovData = nytData[0]["response"]["docs"]
-			.map(obj => {
-				let url = obj["web_url"];
-				let header = obj["abstract"];
-				let date = Date.parse(obj["pub_date"]);
-				return [date, `<a href="${url}" target="_blank">${header}</a><br/>`, "NYTimes"];
-			});
+			.map(obj => toLink(obj, "web_url", "abstract", "pub_date", "NYTimes"));
 		let concated = cdcNcovData.concat(nytNcovData)
 			.sort((p1, p2) => p2[0] - p1[0])
 			.map(p => `${p[2]}, ${toDate(p[0])} - ${p[1]}`);
